d899d13d55d76be823080958bf89a65f7c8203b9
fusion1110 <krishgulati7@gmail.com>
2024-08-29 00:06:06 +0530

    Leetcode questions

---
+++ palindrome.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int romanToInt(string s)
+{
+    map<char, int> m;
+    m['I'] = 1;
+    m['V'] = 5;
+    m['X'] = 10;
+    m['L'] = 50;
+    m['C'] = 100;
+    m['D'] = 500;
+    m['M'] = 1000;
+
+    int ans = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (i < s.length() - 1 && m[s[i]] < m[s[i + 1]])
+        {
+            ans -= m[s[i]];
+        }
+        else
+        {
+            ans += m[s[i]];
+        }
+    }
+
+    return ans;
+}
+
+// for (int i = 0; i < s.length(); i++)
+// {
+//     if (s[i] == 'I')
+//     {
+//         ans += I;
+//     }
+//     else if (s[i] == 'V')
+//     {
+//         if (i > 0 && s[i - 1] == 'I')
+//             ans += V - 2 * I;
+//         else
+//             ans += V;
+//     }
+//     else if (s[i] == 'X')
+//     {
+//         if (i > 0 && s[i - 1] == 'I')
+//             ans += X - 2 * I;
+//         else
+//             ans += X;
+//     }
+//     else if (s[i] == 'L')
+//     {
+//         if (i > 0 && s[i - 1] == 'X')
+//             ans += L - 2 * X;
+//         else
+//             ans += L;
+//     }
+//     else if (s[i] == 'C')
+//     {
+//         if (i > 0 && s[i - 1] == 'X')
+//             ans += C - 2 * X;
+//         else
+//             ans += C;
+//     }
+//     else if (s[i] == 'D')
+//     {
+//         if (i > 0 && s[i - 1] == 'C')
+//             ans += D - 2 * C;
+//         else
+//             ans += D;
+//     }
+//     else if (s[i] == 'M')
+//     {
+//         if (i > 0 && s[i - 1] == 'C')
+//             ans += M - 2 * C;
+//         else
+//             ans += M;
+//     }
+// }
+
+
+int main()
+{
+    cout << romanToInt("IV") << endl;
+    return 0;
+}
