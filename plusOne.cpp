91d1664a8fbb2133e8a8004b401636f13f7447b8
fusion1110 <krishgulati7@.com>
2024-09-03 01:07:48 +0530

    modified:   plusOne.cpp

--- plusOne.cpp
+++ plusOne.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 vector<int> plusOne(vector<int> &digits)
 {
     int n = digits.size();
 
-    for (int i = n - 1; i >= 0; --i) //starts at the end of the vector/array
+    //--1 is faster than 1--
+    for (int i = n - 1; i >= 0; --i) // starts at the end of the vector/array
     {
         if (digits[i] < 9)
         {
-            digits[i] += 1; //simply add 1 to the last element/number
+            digits[i] += 1; // simply add 1 to the last element/number
             return digits;
         }
         digits[i] = 0; // 9 hua last mai toh usko 0 krna padega
     }
 
-    vector<int> result(n + 1, 0); //end mai 9 tha toh had to add an extra space so created a new vector
+    vector<int> result(n + 1, 0); // end mai 9 tha toh had to add an extra space so created a new vector
     result[0] = 1;
     return result;
 }
