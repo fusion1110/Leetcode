9cc1163aacf33eae77677ef8264e67e695e8d155
fusion1110 <krishgulati7@gmail.com>
2025-04-02 19:29:11 +0530

    modified:   MaximumValueofanOrderedTriplet.cpp

--- MaximumValueofanOrderedTriplet.cpp
+++ MaximumValueofanOrderedTriplet.cpp
@@ -1,24 +1,25 @@
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-long long maximumTripletValue(vector<int>& nums) {
-    //imax is maxValue of nums[i]
-    //dmax is maxValue of nums[i] - nums[j]
+long long maximumTripletValue(vector<int> &nums)
+{
+    // imax is maxValue of nums[i]
+    // dmax is maxValue of nums[i] - nums[j]
     long long maxValue = 0, imax = 0, dmax = 0;
-    for(int k = 0; k < nums.size(); k++){
+    for (int k = 0; k < nums.size(); k++)
+    {
         maxValue = max(maxValue, dmax * nums[k]);
         dmax = max(dmax, imax - nums[k]);
-        //C++ type conversion
+        // C++ type conversion
         imax = max(imax, static_cast<long long>(nums[k]));
     }
     return maxValue;
 }
 
-int main() {
-    // Sample test case
-    vector<int> nums = {3, 7, 1, 5, 10};
+int main()
+{
 
-    // Calling the function and displaying the result
+    vector<int> nums = {3, 7, 1, 5, 10};
     cout << "Maximum Triplet Value: " << maximumTripletValue(nums) << endl;
 
     return 0;
